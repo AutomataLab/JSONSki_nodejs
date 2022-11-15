d7df137e7b6a94b1bc040e0a731024513f8c5b65
Gandharva Deshpande <gandharvadeshpande@Gandharvas-MBP.lan>
2022-11-14 23:25:21 -0800

    adding keywords

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-const JSki = require('./build/Release/JSONSki.node')
-var fs = require('fs');
-console.log("C++ JSONSKI")
-console.time();
-console.log('JsonSki Runtime', JSki.JSONSkiParser("$[*].entities.urls[*].url", "dataset/twitter_sample_large_record.json"));
-console.timeEnd();
-contents = fs.readFileSync('dataset/twitter_sample_large_record.json')
-c = contents.toString()
-console.log("Javascript Runtime ")
-console.time();
-var json = JSON.parse(c);
-console.timeEnd();
-module.exports = JSki;
+const jsonski = require('./build/Release/JSONSki.node')
+module.exports = jsonski;
